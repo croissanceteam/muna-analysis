26798fda722fea3d43c91dad877948e633150584
LamaDev <hlama@live.fr>
2019-02-26 11:10:01 +0100

    Fix message error for 500 code HTTP

--- src/main/java/com/datavisualzer/regideso/web/ErrorControllerViewer.java
+++ src/main/java/com/datavisualzer/regideso/web/ErrorControllerViewer.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ErrorControllerViewer implements ErrorController{
 
     @RequestMapping(value="/error")
-    public String errorviewpage(HttpServletRequest request){
+    public String errorviewpage(HttpServletRequest request,ModelMap model){
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
      
     if (status != null) {
@@ -25,6 +26,7 @@ public class ErrorControllerViewer implements ErrorController{
             return "views/errors/error-404";
         }
         else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            model.put("message",HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
             return "views/errors/error-500";
         }
     }
