650efd13ba6fd22625ba976bb7765cd4787d6b87
LamaDev <hlama@live.fr>
2019-02-28 12:22:53 +0100

    Fix bug Export Excel Fix3

--- src/main/java/com/datavisualzer/regideso/web/ExportController.java
+++ src/main/java/com/datavisualzer/regideso/web/ExportController.java
@@ -2,6 +2,8 @@ package com.datavisualzer.regideso.web;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
@@ -13,6 +15,15 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -96,5 +107,43 @@ public class ExportController {
 		}
 	
 		return null;
+	}
+	@RequestMapping(value="/download/excel",method=RequestMethod.GET)
+	public StreamingResponseBody getExcel(HttpServletResponse response) throws IOException {
+		response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+		response.setHeader("Content-Disposition", "attachment; filename="+result);
+		Workbook workbook = new XSSFWorkbook();
+		 
+		Sheet sheet = workbook.createSheet("Persons");
+		sheet.setColumnWidth(0, 6000);
+		sheet.setColumnWidth(1, 4000);
+		 
+		Row header = sheet.createRow(0);
+		 
+		CellStyle headerStyle = workbook.createCellStyle();
+		headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		 
+		XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+		font.setFontName("Arial");
+		font.setFontHeightInPoints((short) 16);
+		font.setBold(true);
+		headerStyle.setFont(font);
+		 
+		Cell headerCell = header.createCell(0);
+		headerCell.setCellValue("Name");
+		headerCell.setCellStyle(headerStyle);
+		 
+		headerCell = header.createCell(1);
+		headerCell.setCellValue("Age");
+		headerCell.setCellStyle(headerStyle);
+		File currDir = new File(".");
+		String path = currDir.getAbsolutePath();
+		String fileLocation = path.substring(0, path.length() - 1) + "temp.xlsx";
+		 
+		FileOutputStream outputStream = new FileOutputStream(fileLocation);
+		workbook.write(outputStream);
+		workbook.close();
+		return (StreamingResponseBody) outputStream;
 	}
 }
