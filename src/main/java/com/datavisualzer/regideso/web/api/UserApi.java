ffa278875e96dc337ca02aebea30953615433a9a
LamaDev <hlama@live.fr>
2019-06-23 02:05:37 +0100

    Fix textes landing page and create Api Auth moblie client

--- src/main/java/com/datavisualzer/regideso/web/api/UserApi.java
+++ src/main/java/com/datavisualzer/regideso/web/api/UserApi.java
@@ -4,19 +4,30 @@ import com.datavisualzer.regideso.models.Factory;
 import com.datavisualzer.regideso.models.PermissionTypeFileOrgunitUser;
 import com.datavisualzer.regideso.models.Permissions;
 import com.datavisualzer.regideso.models.Users;
+import com.datavisualzer.regideso.process.CryptoSHA512;
 import com.datavisualzer.regideso.repositories.FactoryRepository;
 import com.datavisualzer.regideso.repositories.PermissionRepository;
 import com.datavisualzer.regideso.repositories.PermissionTypeFileOrgunitUserRepository;
 import com.datavisualzer.regideso.repositories.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javassist.expr.NewArray;
+
+import org.apache.commons.collections4.map.HashedMap;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+
+import java.security.NoSuchAlgorithmException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class UserApi {
@@ -70,5 +81,17 @@ public class UserApi {
     @GetMapping("api/permissions")
     public List<PermissionTypeFileOrgunitUser> getUsersPermissions(){
         return (List<PermissionTypeFileOrgunitUser>)permissionRepository.findAll();
+    }
+    
+    @PostMapping("api/login")
+    public Object LoggerMobile(@RequestParam String username,@RequestParam String password) throws Exception {
+    	Users userAuthenticated=userRepository.findByusernameAndpassword(username,CryptoSHA512.getPassWord(password));
+    	if (userAuthenticated!=null) {
+			//ObjectMapper mapper=new ObjectMapper();
+			return permissionRepository.findByidUsers(userAuthenticated);
+		} 
+    	Map map=new HashedMap<String,Integer>();
+    	map.put("id", 0);
+    	return map;
     }
 }
